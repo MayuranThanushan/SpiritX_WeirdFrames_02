ba4d10103fe16f1476ec09d5ca06d160a0e3cb8d
Mayuran Thanushan <mayuranthanushan@gmail.com>
2025-03-09 19:40:43 +0530

    add keys

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Missing Supabase environment variables');
-}
+const supabaseUrl = 'https://qjhynilecxatbqybqalr.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqaHluaWxlY3hhdGJxeWJxYWxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE1MjgxMDcsImV4cCI6MjA1NzEwNDEwN30.iMxWdrisC5V9_vX_cuYQiWxLCu7jcdJdm8TbTacx3Qg';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
