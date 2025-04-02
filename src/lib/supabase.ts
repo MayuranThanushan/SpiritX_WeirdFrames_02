df79c342cfdec9e02f1cf154a584d51015c98182
MayuranThanushan <mayuranthanushan@gmail.com>
2025-04-03 05:13:16 +0530

    removef public keys

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
 
-const supabaseUrl = 'https://qjhynilecxatbqybqalr.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFqaHluaWxlY3hhdGJxeWJxYWxyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE1MjgxMDcsImV4cCI6MjA1NzEwNDEwN30.iMxWdrisC5V9_vX_cuYQiWxLCu7jcdJdm8TbTacx3Qg';
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
